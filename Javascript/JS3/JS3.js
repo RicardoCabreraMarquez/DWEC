df2f6a746b3f59e6742eafea08ad66327a9d9597
RicardoCabreraMarquez <95092615+RicardoCabreraMarquez@users.noreply.github.com>
2022-11-24 17:42:57 +0100

    .

---
+++ Javascript/JS3/JS3.js
@@ -0,0 +1,6 @@
+function pintar(){
+    document.getElementsByClassName(b-horizontal).innerHTML = `style="background-color: red;`;
+    const horizontal = document.getElementById("b-vertical");
+
+    
+}
