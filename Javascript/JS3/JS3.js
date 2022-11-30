6f49517c042d694df588171a5ccdb70aa12b0313
RicardoCabreraMarquez <95092615+RicardoCabreraMarquez@users.noreply.github.com>
2022-11-30 20:57:03 +0100

    .

--- Javascript/JS3/JS3.js
+++ Javascript/JS3/JS3.js
@@ -1,6 +1,15 @@
 function pintar(){
-    document.getElementsByClassName(b-horizontal).innerHTML = `style="background-color: red;`;
-    const horizontal = document.getElementById("b-vertical");
+    document.getElementById('obj1').style.cssText = 'background-color: red;';
+    document.getElementById('obj2').style.cssText = 'background-color: red;';
+    document.getElementById('obj3').style.cssText = 'background-color: red;';
+    document.getElementById('obj4').style.cssText = 'background-color: red;';
+    document.getElementById('obj5').style.cssText = 'background-color: red;';
+    document.getElementById('obj6').style.cssText = 'background-color: red;';
+    document.getElementById('obj7').style.cssText = 'background-color: red;';
+    document.getElementById('obj8').style.cssText = 'background-color: red;';
+    document.getElementById('obj9').style.cssText = 'background-color: red;';
+    document.getElementById('obj10').style.cssText = 'background-color: red;';
+    document.getElementById('obj11').style.cssText = 'background-color: red;';
+    document.getElementById('obj12').style.cssText = 'background-color: red;';
 
-    
 }
