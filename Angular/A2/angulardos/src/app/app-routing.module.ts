c82092447bc1f90af3415c03a8b01aa632ac0f32
RicardoCabreraMarquez <95092615+RicardoCabreraMarquez@users.noreply.github.com>
2023-02-07 18:54:13 +0100

    .

--- Angular/A2/angulardos/src/app/app-routing.module.ts
+++ Angular/A2/angulardos/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginpagesComponent } from './pages/loginpages/loginpages.component';
+import { RegisterpagesComponent } from './pages/registerpages/registerpages.component';
 
 const routes: Routes = [
+  {path: 'loginpages',  component: LoginpagesComponent},
+  {path: 'registerpages',  component: RegisterpagesComponent},
   { path: '', redirectTo: '/loginpages', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],exports: [RouterModule]
 })
 export class AppRoutingModule { }
